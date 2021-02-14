3212a4bb535dd35af9c85d40fa000a149af8db7d
Ratul <azammmgol@gmail.com>
2021-02-14 16:15:08 +0600

    aorking on fuctions

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ const mainCard = document.querySelector('#ContentWarpper')
 const songImg = document.querySelector('#SongImg')
 const controlButtons = document.querySelector('.control')
 
+const playPauseButton = document.querySelector('#PausePlay')
+const audio = document.querySelector('audio')
+
+playPauseButton.addEventListener('click', () => {
+  audio.play()
+  playPauseButton.classList.replace('fa-play', 'fa-pause')
+  songImg.classList.add('anime')
+})
+
 mainCard.addEventListener('mouseover', (event) => {
   const xAxis = (window.innerWidth / 2 - event.pageX) / 15
   const yAxis = (window.innerHeight / 2 - event.pageY) / 15
   mainCard.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`
   songImg.style.transform = `rotate(${xAxis}deg)`
-  controlButtons.style.transform = `rotate(${yAxis}deg)`
+  controlButtons.style.transform = `rotate(${xAxis}deg)`
 })
 mainCard.addEventListener('mouseleave', () => {
   mainCard.style.transform = 'rotateX(0deg) rotateY(0deg)'
   songImg.style.transform = 'rotate(0deg)'
+  controlButtons.style.transform = 'rotate(0deg)'
 })
