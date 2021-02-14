371cc047d31c433d1c10ff981623a65ca9b76a13
Ratul <azammmgol@gmail.com>
2021-02-14 17:06:44 +0600

    all fuctions ready

--- script.js
+++ script.js
@@ -4,7 +4,65 @@ const controlButtons = document.querySelector('.control')
 
 const playPauseButton = document.querySelector('#PausePlay')
 const audio = document.querySelector('audio')
+const artist = document.querySelector('#Artist')
+const songName = document.querySelector('#SongName')
+const previousButton = document.querySelector('#Previous')
+const nextButton = document.querySelector('#Next')
+const songImgAtTheTop = document.querySelector('img')
+
 let isPlaying = false
+let index = 0
+
+const songDataBase = [
+  {
+    songSrc: './music/music1.mp3',
+    title: 'A Different Way',
+    artist: 'Lauv',
+    imgSrc: './img/music1.jpg',
+  },
+  {
+    songSrc: './music/music2.mp3',
+    title: 'Butterfly Effect',
+    artist: 'Dj Snake',
+    imgSrc: './img/music2.jpg',
+  },
+  {
+    songSrc: './music/music3.mp3',
+    title: 'Ghashi',
+    artist: 'Carte Blanche',
+    imgSrc: './img/music3.jpg',
+  },
+  {
+    songSrc: './music/music4.mp3',
+    title: 'Mohammad Nabina',
+    artist: 'Unknown',
+    imgSrc: './img/music4.jpg',
+  },
+]
+
+const loadMusic = () => {
+  audio.src = songDataBase[index].songSrc
+  artist.textContent = songDataBase[index].artist
+  songName.textContent = songDataBase[index].title
+  songImgAtTheTop.src = songDataBase[index].imgSrc
+}
+
+loadMusic()
+
+nextButton.addEventListener('click', () => {
+  if (index <= songDataBase.length) {
+    loadMusic(index++)
+    play()
+  } else {
+    pause()
+  }
+})
+previousButton.addEventListener('click', () => {
+  if (index >= 0) {
+    loadMusic(index--)
+    play()
+  }
+})
 
 const play = () => {
   isPlaying = true
